915403c59d1f666c53577170cab1798016d95d96
HANYUJIN <mooo7570@gmail.com>
2024-02-16 00:32:23 +0900

    fix: Image remotePatterns 외부 이미지 경로 설정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+    ],
   },
 }
 
