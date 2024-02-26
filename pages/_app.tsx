c821ca651163cfc49c6e46abf62f7f62b16c0e45
HANYUJIN <mooo7570@gmail.com>
2024-02-27 04:52:55 +0900

    title 전역 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <title>한유진 포트폴리오</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
